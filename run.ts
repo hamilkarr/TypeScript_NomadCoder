f444f1468b9c69f2063a6a4546b12482f1436644
hamilkarr <12uclock@gmail.com>
2024-03-18 23:55:51 +0900

    코드 변경 내용 추가

--- run.ts
+++ run.ts
@@ -1,1 +1,28 @@
 const player: readonly [string, number, boolean] = ['nico', 1, true];
+
+let a: unknown;
+
+if (typeof a === 'number') {
+    let b = a + 1;
+}
+
+if (typeof a === 'string') {
+    let b = a.toUpperCase();
+}
+
+function hello() {
+    console.log('x');
+}
+
+const c = hello();
+c.toUpperCase();
+
+function hello2(name: string | number) {
+    if (typeof name === 'string') {
+        name;
+    } else if (typeof name === 'number') {
+        name;
+    } else {
+        name;
+    }
+}
