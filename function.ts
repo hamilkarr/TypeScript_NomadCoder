1a168f9e219920785919a4f92d265f84739b7fc7
hamilkarr <12uclock@gmail.com>
2024-03-24 22:16:48 +0900

    타입 변경 및 함수명 변경

--- function.ts
+++ function.ts
@@ -1,12 +1,7 @@
-type Add = {
-    (a: number, b: number): number;
-    (a: number, b: number, c: number): number;
-};
+type SuperPrint = <T, M>(arr: T[], b: M) => T;
 
-const add: Add = (a: number, b: number, c?: number) => {
-    if (c) return a + b + c;
-    return a + b;
-};
-
-add(1, 2); // 3
-add(1, 2, 3); // 6
+const superPrint: SuperPrint = (arr) => arr[0];
+superPrint([1, 2, 3, 4], 'X');
+// superPrint(['a', 'b', 'c', 'd']);
+// superPrint([true, false, true, false]);
+// superPrint([1, 2, true, 'hello']);
