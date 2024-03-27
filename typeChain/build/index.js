a2dce72015a7b4bc0b4ca29df7d714df8c9bb6b3
hamilkarr <12uclock@gmail.com>
2024-03-28 00:22:48 +0900

    커밋: 라이브러리 선언 추가 및 초기화 함수 호출

--- typeChain/build/index.js
+++ typeChain/build/index.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const myPackage_1 = require("myPackage");
+(0, myPackage_1.init)({
+    url: 'true',
+});
+(0, myPackage_1.exit)(1);
